b931e3ee919929269d8d6fbfbd229b79b80af6ae
NandhiniR26 <nandhinirameshrk@gmail.com>
2024-07-24 15:04:29 +0530

    Done with connectng to database and Starting the server

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-//Connect to database
+const {MONGODB_URI}= require('./utils/config');
+const app = require('./app');
 
-//Start the server
+//Connect to database
+const mongoose = require("mongoose");
+
+mongoose.connect(MONGODB_URI)
+     .then(()=> {
+        console.log('Connected to MongoDB..')
+    //Start the server
+app.listen(3001,()=>{
+    console.log('Server is running on port 3001')
+})
+    })
+     .catch(err=>console.log('Could not connect to MongoDB..',err))
+
+

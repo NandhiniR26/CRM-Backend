8c25cd33c6f66a50c9d66f4b60f11c61927d1a90
NandhiniR26 <nandhinirameshrk@gmail.com>
2024-07-29 11:49:21 +0530

    auth system setup done

---
+++ readme.md
@@ -0,0 +1,5 @@
+
+# CRM Application  
+
+
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,14 @@
 //create a router
 const express = require('express');
 const userRouter = express.Router();
-const userController=require('../controllers/userController')
+const userController=require('../controllers/userController');
+const auth = require('../middleware/auth');
 
 //add routes
-userRouter.get('/',userController.getData);
-
+// userRouter.get('/',userController.getData);
+userRouter.post('/register',userController.register);
+userRouter.post('/login',userController.login);
+userRouter.get('/logout',userController.logout);
+userRouter.get('/profile',auth.verifyToken,userController.getProfile);
 //export the router
 module.exports = userRouter;

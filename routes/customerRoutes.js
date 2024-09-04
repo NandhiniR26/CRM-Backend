5c5dd6db7ebb0cfc4286e59402ec910be0f2a0b2
NandhiniR26 <nandhinirameshrk@gmail.com>
2024-09-04 08:59:51 +0530

    modified customer routes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -4,7 +4,7 @@ const customerRouter=express.Router();
 const auth = require('../middleware/auth');
 
 customerRouter.post('/',auth.verifyToken,auth.isAdmin,customerController.createCustomer);
-customerRouter.get('/',auth.verifyToken,customerController.getAllCustomers);
+customerRouter.get('/',customerController.getAllCustomers);
 customerRouter.get('/:id',auth.verifyToken,customerController.getCustomerById);
 customerRouter.put('/:id',auth.verifyToken,auth.isAdmin,customerController.updateCustomer);
 customerRouter.delete('/:id',auth.verifyToken,auth.isAdmin,customerController.deleteCustomer);
